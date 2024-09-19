631d6a372c1747065d4400377c8f4b1b5eafa6af
Vivek Kumar <vivekkumarbeg7@gmail.com>
2024-09-19 12:36:01 +0530

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
             return;
         } else {
             // make api call to our backend. we'll leave thisfor later
-            axios.post("https://www.brahama.work.gd/api/loginTest", {
+            axios.post("http://localhost:8000/loginTest", {
                 username: username,
                 password: password,
             })
